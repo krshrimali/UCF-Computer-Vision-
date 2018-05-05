347a361c1294b51937eb9afc47d02bd025d5e269
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2018-05-05 13:07:28 +0530

    Added CDF Histogram Equalization

--- pixel_intensity_plot.py
+++ pixel_intensity_plot.py
@@ -6,6 +6,49 @@ import random
 import sys
 import math as m
 
+def cumsum(h):
+    # credits:://gist.github.com/bistaumanga/6309599 
+    list_ = []
+    for i in range(len(h)):
+        list_.append(sum(h[:i+1]))
+    
+    print(i, len(h))
+    print("FINISHED")
+    # return [sum(h[:i+1] for i in range(len(h)))]
+    return list_
+
+def imhist(img):
+    # normalized histogram of image
+    # credits:://gist.github.com/bistaumanga/6309599 
+
+    m, n = img.shape
+    h    = [0.0] * 256 # create 256 length array, zero elements
+    
+    for i in range(m):
+        for j in range(n):
+            h[img[i, j]] += 1
+    
+    return np.array(h)/(m*n)
+
+def equalize_hist(img):
+    img         = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    print(img.shape[0], img.shape[1])
+    # credits:://gist.github.com/bistaumanga/6309599 
+    h           = imhist(img)
+    cdf         = np.array(cumsum(h))
+    sk          = np.uint8(255 * cdf)
+    s1, s2      = img.shape
+    Y           = np.zeros_like(img)
+    # applying transferred values for each pixels
+    for i in range(0, s1):
+        for j in range(0, s2):
+            Y[i, j] = sk[img[i, j]]
+
+    H = imhist(Y)
+
+    # return transformed image, original new histogram, transform function
+    return Y, h, H, sk
+
 def draw_hist(images):
     img  = images[0]
     img2 = images[1]
@@ -46,38 +89,38 @@ def draw_hist(images):
                 if(k == 0):
                     channel = 'Blue'
                     plt.subplot(131)
-                    plt.plot(histogram, color='b', label='BLUE CHANNEL')
+                    plt.plot(histogram, color='b', label='B')
                     plt.legend(loc="upper right")
         
                 elif(k == 1):
                     channel = 'Green'
                     plt.subplot(132)
-                    plt.plot(histogram, color='g', label='GREEN CHANNEL')
+                    plt.plot(histogram, color='g', label='G')
                     plt.legend(loc="upper right")
     
                 elif(k == 2):
                     channel = 'Red'
                     plt.subplot(133)
-                    plt.plot(histogram, color='r', label='RED CHANNEL')
+                    plt.plot(histogram, color='r', label='R')
                     plt.legend(loc="upper right")
     
             if index == 1:
                 if(k == 0):
                     channel = 'Blue'
                     plt.subplot(131)
-                    plt.plot(histogram2, color = 'b', ls = '--', label='BLUE CHANNEL(NEW)')
+                    plt.plot(histogram2, color = 'b', ls = '--', label='B(Noisy)')
                     plt.legend(loc="upper right")
         
                 elif(k == 1):
                     channel = 'Green'
                     plt.subplot(132)
-                    plt.plot(histogram2, color = 'g', ls = '--', label='GREEN CHANNEL(NEW)')
+                    plt.plot(histogram2, color = 'g', ls = '--', label='G(Noisy)')
                     plt.legend(loc="upper right")
     
                 elif(k == 2):
                     channel = 'Red'
                     plt.subplot(133)
-                    plt.plot(histogram2, color = 'r', ls = '--', label='RED CHANNEL(NEW)')
+                    plt.plot(histogram2, color = 'r', ls = '--', label='R(Noisy)')
                     plt.legend(loc="upper right")
     
                 # plt.imshow("Plot")    
@@ -144,7 +187,7 @@ rows = img.shape[0]
 cols = img.shape[1]
 
 # img2 = gamma_correction(img)
-
+'''
 noise_or_not = input("You want to add noise to the image?")
 if(noise_or_not.lower() == "yes"):
     img2 = add_noise(img)
@@ -157,7 +200,22 @@ else:
     plt.imshow(img2)
     plt.show()
     draw_hist([img, img2])
+'''
 
+print("Now applying cdf to the original image, histogram equalization ")
+new_img, original_hist, new_hist, transform_func = equalize_hist(img)
+
+cv2.imshow("new_img", new_img)
+cv2.imwrite("new_img.png", new_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+plt.plot(original_hist, color = 'b', ls = '--', label = 'Original Histogram')
+plt.legend(loc="upper right")
+plt.plot(new_hist, color = 'r', label = 'New Histogram')
+plt.legend(loc="upper right")
+
+plt.show()
 '''
 plt.imshow(img2)
 plt.show()
